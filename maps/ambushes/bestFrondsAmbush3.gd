e772981927ed1ec411eaafaf0289a98b6edc44a4
krizby <krizby425@gmail.com>
2022-06-21 14:54:20 -0700

    balance changes to level 1; music bug fixes

--- maps/ambushes/bestFrondsAmbush3.gd
+++ maps/ambushes/bestFrondsAmbush3.gd
@@ -17,23 +17,23 @@ var numWaves = 3
 var wave0 = [
 	nme.ambEnemy.new(nme.greenMantisScene,   nme.greenMantis,   Vector3(26, 0, 0),  Vector3.ZERO, 30),
 	nme.ambEnemy.new(nme.greenMantisScene,   nme.greenMantis,   Vector3(-26, 0, 0),  Vector3.ZERO, 30),
-	nme.ambEnemy.new(nme.greenMantisScene,   nme.greenMantis,   Vector3(0, -1.5, 3),  Vector3(0, 15, -2), 60)
+	nme.ambEnemy.new(nme.spiderScene,   nme.spider,   Vector3(0, -1.5, 3),  Vector3(0, 15, -2), 60)
 ]
 var wave1 = [
 	nme.ambEnemy.new(nme.greenMantisScene,   nme.greenMantis,   Vector3(0, -1.5, 3),  Vector3(0, 25, -2), 60),
-	nme.ambEnemy.new(nme.spiderScene,   nme.spider,   Vector3(26, 0, -5),  Vector3.ZERO, 0),
+	#nme.ambEnemy.new(nme.spiderScene,   nme.spider,   Vector3(26, 0, -5),  Vector3.ZERO, 0),
 	nme.ambEnemy.new(nme.spiderScene,   nme.spider,   Vector3(26, 0, -5),  Vector3.ZERO, 30),
 	nme.ambEnemy.new(nme.spiderScene,   nme.spider,   Vector3(26, 0, -5),  Vector3.ZERO, 60),
-	nme.ambEnemy.new(nme.spiderScene,   nme.spider,   Vector3(-26, 0, -5),  Vector3.ZERO, 0),
+	#nme.ambEnemy.new(nme.spiderScene,   nme.spider,   Vector3(-26, 0, -5),  Vector3.ZERO, 0),
 	nme.ambEnemy.new(nme.spiderScene,   nme.spider,   Vector3(-26, 0, -5),  Vector3.ZERO, 30),
 	nme.ambEnemy.new(nme.spiderScene,   nme.spider,   Vector3(-26, 0, -5),  Vector3.ZERO, 60)
 ]
 var wave2 = [
 	nme.ambEnemy.new(nme.waspScene,   nme.wasp,   Vector3(0, 4, -12),  Vector3.ZERO, 60),
 	nme.ambEnemy.new(nme.waspScene,   nme.wasp,   Vector3(26, 0, -5),  Vector3.ZERO, 0),
-	nme.ambEnemy.new(nme.waspScene,   nme.wasp,   Vector3(26, 0, -5),  Vector3.ZERO, 30),
+	#nme.ambEnemy.new(nme.waspScene,   nme.wasp,   Vector3(26, 0, -5),  Vector3.ZERO, 30),
 	nme.ambEnemy.new(nme.waspScene,   nme.wasp,   Vector3(-26, 0, -5),  Vector3.ZERO, 0),
-	nme.ambEnemy.new(nme.waspScene,   nme.wasp,   Vector3(-26, 0, -5),  Vector3.ZERO, 30)
+	#nme.ambEnemy.new(nme.waspScene,   nme.wasp,   Vector3(-26, 0, -5),  Vector3.ZERO, 30)
 ]
 
 # Called when the node enters the scene tree for the first time.
