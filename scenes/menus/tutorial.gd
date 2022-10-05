0f534b0f1179aa44c9a0eca1b4dbd0a2e79c01fa
krizby425 <krizby425@gmail.com>
2022-10-04 21:47:51 -0700

    updated tutorial to match new inputs and prevent auto advancing

--- scenes/menus/tutorial.gd
+++ scenes/menus/tutorial.gd
@@ -5,10 +5,15 @@ var messageLength = 0
 var messageText = ""
 onready var messageNode = get_node("label")
 
+var animationNum = -3
+
+var skippable = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("startButton").grab_focus()
 	soundManager.playMusicIfDiff("tutorial")
+	$"arrowContinue".hide()
 
 
 func _process(delta):
@@ -19,13 +24,25 @@ func _process(delta):
 		messagePercent += delta * (100 / (messageLength * 0.025))
 	if (messagePercent > 100):
 		messagePercent = 100
+		$"arrowContinue".show()
 	messageNode.percent_visible = (messagePercent* 0.01)
 	# skips
 	if (Input.is_action_just_pressed("pause") == true):
 		tran.loadLevel("res://scenes/menus/mapOpen.tscn")
 	if (Input.is_action_just_pressed("ui_accept") == true):
-		var animationNum = get_node("AnimationPlayer").current_animation
-		_on_AnimationPlayer_animation_finished(animationNum)
+#		var animationNum = get_node("AnimationPlayer").current_animation
+#		_on_AnimationPlayer_animation_finished(animationNum)
+		advance()
+		
+func advance():
+	if not skippable:
+		return
+	elif (animationNum == 16):
+		finished()
+	else:
+		animationNum = animationNum + 1
+		$"AnimationPlayer".play(str(animationNum))
+		$"arrowContinue".hide()
 
 func speak(dialogue):
 	messagePercent = 0
@@ -38,11 +55,12 @@ func _on_startButton_pressed():
 	#tran.loadLevel("res://scenes/menus/mapOpen.tscn")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	if (anim_name == "16"):
-		finished()
-	else:
-		var nextAnim = int(anim_name) + 1
-		$"AnimationPlayer".play(str(nextAnim))
+	skippable = true
+#	if (anim_name == "16"):
+#		finished()
+#	else:
+#		var nextAnim = int(anim_name) + 1
+#		$"AnimationPlayer".play(str(nextAnim))
 	
 func finished():
 	$"AnimationPlayer".stop()
