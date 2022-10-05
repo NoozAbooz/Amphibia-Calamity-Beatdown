1e0df0901d395b2f05caeb6f707a6f49562fdfa8
krizby425 <krizby425@gmail.com>
2022-10-04 22:11:21 -0700

    challenge level now grants two unlockable moves

---
+++ maps/bestFrondsHard.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pg.hasCounter = true
+	pg.hasSpin = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
